e18d675cbd774915acd9cd265eb7754ed25253a5
AtilaLima/Unifametro <atilalimade@gmail.com>
2023-10-24 08:06:43 -0300

    Primeira tentativa para contar o timer, muito crua

--- afklolprevent.py
+++ afklolprevent.py
@@ -1,7 +1,7 @@
 #1.2: fixed timer to display real time spent afk.
 
 import pyautogui as pag
-import time; from random import SystemRandom
+import time; from random import SystemRandom, randint
 from logging import debug, info, warning
 
 InitArea = pag.size() #basic resolution
@@ -22,7 +22,7 @@ print('')
 
 #vars to init
 myPos = pag.position()
-afk_timer = 0
+afk_timer = 18  #to change for debugging
 time_displayed = int(afk_timer)
 
 
@@ -30,23 +30,25 @@ time_displayed = int(afk_timer)
 while True:
     if afk_timer >= 14400: break
     if pag.position() == myPos: #if mousestopped
-        afk_timer += 2
+        afk_timer += 2  #default, every 2 seconds will iterate
     else:
         afk_timer = 0
         myPos = pag.position()
         info("Mouse moved, not afk.")
-        
+
+    if afk_timer == 20:
+        info("Afk enabled.") #info only when started
+
     if afk_timer >= 240:
         warning("2 minutes time exceded: afk disabled")    
     elif afk_timer >= 20:
         #calc area to be used of the clicks
         myAreaX, myAreaY = pag.size() #if area changes, will still use this to get the area
-        Xrange = [round((myAreaX/5) * 2), round((myAreaX/5) * 3)]
-        Yrange = [round(myAreaY/4 * 2), round((myAreaY/4) * 3)]
+        Xrange = [round( (myAreaX/5) * 2 ), round( (myAreaX/5) * 3 )]
+        Yrange = [round( (myAreaY/4) * 2 ), round( (myAreaY/4) * 3 )]
 
         #execute the clicks
-        floatsChoice = (SystemRandom().uniform(0, 1)) #secure random float
-        info("Afk enabled.")
+        floatsChoice = (SystemRandom().uniform(0, 3)) #secure random float
         xVar = randint(Xrange[0], Xrange[1])
         yVar = randint(Yrange[0], Yrange[1])
         pag.click(button="right", x=xVar, y=yVar)
@@ -54,8 +56,10 @@ while True:
         pag.hotkey("S")
         myPos = pag.position()
 
-        time_displayed += (2 + floatsChoice)
+        debug(floatsChoice)
+        time_displayed += (2 + floatsChoice) #? use time.perfcounter()
         debug(f'Time stoped: {time_displayed:,.1f} seconds(so or less).'.format(time_displayed))
+    
     print('')
     time.sleep(2)
 
